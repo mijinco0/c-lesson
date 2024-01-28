b15060ba2eea50b405ad7c17325aed1b2a7e5dac
mijinco0 <0106@retrotecture.jp>
2024-01-28 15:10:16 +0900

    Add mydata_init_integer(), mydata_init_string(), and typedef of MyDataType

--- sources/forth_modoki/04_stack/mydata.c
+++ sources/forth_modoki/04_stack/mydata.c
@@ -1,5 +1,39 @@
 #include <stdlib.h>
+#include <string.h>
 #include "mydata.h"
+
+mydata_t *mydata_init_integer(int i)
+{
+    mydata_t *d = mydata_alloc();
+    if (!d) return NULL;
+
+    d->type = INTEGER;
+    d->data = (void *)malloc(sizeof(int));
+    if (!d->data) {
+        free(d);
+        return NULL;
+    }
+    *(int *)d->data = i;
+
+    return d;
+}
+
+mydata_t *mydata_init_string(char *s)
+{
+    mydata_t *d = mydata_alloc();
+    if (!d) return NULL;
+
+    size_t n = strlen(s) + 1;
+    d->type = STRING;
+    d->data = (void *)malloc(sizeof(char) * n);
+    if (!d->data) {
+        free(d);
+        return NULL;
+    }
+    strncpy((char *)d->data, s, n);
+
+    return d;
+}
 
 mydata_t *mydata_alloc(void)
 {
--- sources/forth_modoki/04_stack/mydata.h
+++ sources/forth_modoki/04_stack/mydata.h
@@ -1,17 +1,20 @@
+
 #ifndef _MYDATA_H_
 #define _MYDATA_H_
 
-enum MyDataType {
+typedef enum MyDataType {
     INTEGER,
     STRING,
     UNKNOWN,
-};
+} mydata_type_t;
 
 typedef struct MyData {
     enum MyDataType type;
     void *data;
 } mydata_t;
 
+mydata_t *mydata_init_integer(int i);
+mydata_t *mydata_init_string(char *s);
 mydata_t *mydata_alloc(void);
 void mydata_free(mydata_t *d);
 
