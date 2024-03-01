09b96c4ca30332992ede13df831b0ccd86b78ea7
mijinco0 <0106@retrotecture.jp>
2024-03-02 08:38:36 +0900

    Change parameter name of stkelm_* functions from 'd' to 'e'

--- sources/forth_modoki/interpreter/stkelm.h
+++ sources/forth_modoki/interpreter/stkelm.h
@@ -15,14 +15,14 @@ typedef struct Stkelm {
 } stkelm_t;
 
 stkelm_t *stkelm_new_integer(int i);
-stkelm_t *stkelm_set_integer(stkelm_t *d, int i);
-stkelm_t *stkelm_new_string(char *s);
-stkelm_t *stkelm_set_string(stkelm_t *d, char *s);
+stkelm_t *stkelm_set_integer(stkelm_t *e, int i);
+stkelm_t *stkelm_new_string(char *e);
+stkelm_t *stkelm_set_string(stkelm_t *e, char *s);
 stkelm_t *stkelm_new(void);
-void stkelm_delete(stkelm_t *d);
+void stkelm_delete(stkelm_t *e);
 stkelm_t *stkelm_duplicate(stkelm_t *src);
-char *stkelm_tostr(char *out, stkelm_t *d, size_t n);
-char *stkelm_get_type(stkelm_t *d);
+char *stkelm_tostr(char *out, stkelm_t *e, size_t n);
+char *stkelm_get_type(stkelm_t *e);
 void stkelm_print(stkelm_t *e, char *prefix, char *suffix);
 
 #endif
